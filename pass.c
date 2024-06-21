fa8ff3bd14d1ed7d897116811b9ef710daf88f48
atr <atrpwq@proton.me>
2024-06-22 00:55:07 +0300

    Add files via upload

--- pass.c
+++ pass.c
@@ -3,17 +3,16 @@
 #include<time.h>
 int pass(int length){
 	srand(time(NULL));
-	if(length!=0){
-		for(int i=0;i<=length;i++){
-			printf("%c",'~'-rand()%105);
-		}
-	}
-	else{
-		printf("NULL");
-		exit(1);
+	for(int i=0;i<=length;i++){
+		printf("%c",'~'-rand()%105);
 	}
 	printf("\n");
 }
 void main(int agrc, char *argv[]){
-	pass(atoi(argv[1]));
+	if(argv[1]!=NULL){
+		pass(atoi(argv[1]));
+	}
+	else{
+		exit(1);
+	}
 }
