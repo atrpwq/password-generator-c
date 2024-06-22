e213e570bb6adec74e519a7326580fc2b4d43a45
atr <atrpwq@proton.me>
2024-06-22 14:35:17 +0300

    Add files via upload

--- pass.c
+++ pass.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
 int pass(int length){
 	srand(time(NULL));
 	for(int i=0;i<=length;i++){
-		printf("%c",'~'-rand()%105);
+		printf("%c",'~'-rand()%104);
 	}
 	printf("\n");
 }
 void main(int agrc, char *argv[]){
+	FILE *file;
 	if(argv[1]!=NULL){
 		pass(atoi(argv[1]));
 	}
